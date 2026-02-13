5ce84102f30d7b7a94ef8ea160cfa10bb0e5567a
Bangyou Zheng <zheng.bangyou@gmail.com>
2026-02-13 11:15:37 +1000

    Update doc

--- _scripts/generate_qmd.R
+++ _scripts/generate_qmd.R
@@ -59,64 +59,62 @@ for (i in seq(along = crops[[1]])) {
         a <- file.remove(files)
     }
     
-    # # for each cultivars
-    # j <- 1
-    # for (j in seq(along = cultivars_names)) {
-    #     cultivar <- cultivars_names[j]
-    #     obs_i <- all_reports |> 
-    #         dplyr::filter(Genotype == tolower(cultivar))
-    #     has_data <- nrow(obs_i) > 0
-    #     # Render .qmd content
-    #     pos <- grep("has_data:", template_cultivar)
-    #     template_cultivar[pos] <- paste0("has_data: ", tolower(as.character(has_data)))
-    #     output <- whisker::whisker.render(template_cultivar, list(
-    #         title = paste(cultivar),
-    #         crop = crop,
-    #         cultivar = cultivar
-    #     ))
-    #     writeLines(output, file.path(crop_output_dir, paste0(cultivar, ".qmd")))
-    # }
+    # for each cultivars
+    j <- 1
+    for (j in seq(along = cultivars_names)) {
+        cultivar <- cultivars_names[j]
+        obs_i <- all_reports |> 
+            dplyr::filter(Genotype == tolower(cultivar))
+        has_data <- nrow(obs_i) > 0
+        # Render .qmd content
+        pos <- grep("has_data:", template_cultivar)
+        template_cultivar[pos] <- paste0("has_data: ", tolower(as.character(has_data)))
+        output <- whisker::whisker.render(template_cultivar, list(
+            title = paste(cultivar),
+            crop = crop,
+            cultivar = cultivar
+        ))
+        writeLines(output, file.path(crop_output_dir, paste0(cultivar, ".qmd")))
+    }
+
+    # Experiments
+    experiments <- all_reports |> 
+        dplyr::distinct(ExperimentName) |> 
+        dplyr::pull(ExperimentName)      
+    if (!is_github) {
+        # local debugging, only keep first 2 cultivars
+        experiments <- experiments[1:2]
+    }
+
+    exp_output_dir <- paste0("crop/", tolower(crop), "/experiment")
+    if (!dir.exists(exp_output_dir)) {
+        dir.create(exp_output_dir, recursive = TRUE)
+    } else {
+        # remove all files in the directory
+        files <- list.files(exp_output_dir, full.names = TRUE)
+        a <- file.remove(files)
+    }
+    for (j in seq(along = experiments)) {
+        experiment_i <- experiments[j]
+        obs_i <- all_reports |> 
+            dplyr::filter(ExperimentName == tolower(experiment_i))
+        # has_data <- nrow(obs_i) > 0
+        # # Render .qmd content
+        # pos <- grep("has_data:", template_cultivar)
+        # template_cultivar[pos] <- paste0("has_data: ", tolower(as.character(has_data)))
+        output <- whisker::whisker.render(template_experiment, list(
+            title = paste(experiment_i),
+            crop = crop,
+            experiment = experiment_i
+        ))
+        writeLines(output, file.path(exp_output_dir, paste0(experiment_i, ".qmd")))
+    }
+
     index_lines_i <- whisker::whisker.render(template_index, list(
             crop = crop
         ))
     writeLines(index_lines_i, file.path(paste0("crop/", tolower(crop)), "index.qmd"))        
 
-    # # Experiments
-    # experiments <- all_reports |> 
-    #     dplyr::distinct(ExperimentName) |> 
-    #     dplyr::pull(ExperimentName)      
-    # if (!is_github) {
-    #     # local debugging, only keep first 2 cultivars
-    #     experiments <- experiments[1:2]
-    # }
-
-    # exp_output_dir <- paste0("crop/", tolower(crop), "/experiment")
-    # if (!dir.exists(exp_output_dir)) {
-    #     dir.create(exp_output_dir, recursive = TRUE)
-    # } else {
-    #     # remove all files in the directory
-    #     files <- list.files(exp_output_dir, full.names = TRUE)
-    #     a <- file.remove(files)
-    # }
-    # for (j in seq(along = experiments)) {
-    #     experiment_i <- experiments[j]
-    #     obs_i <- all_reports |> 
-    #         dplyr::filter(ExperimentName == tolower(experiment_i))
-    #     # has_data <- nrow(obs_i) > 0
-    #     # # Render .qmd content
-    #     # pos <- grep("has_data:", template_cultivar)
-    #     # template_cultivar[pos] <- paste0("has_data: ", tolower(as.character(has_data)))
-    #     output <- whisker::whisker.render(template_experiment, list(
-    #         title = paste(experiment_i),
-    #         crop = crop,
-    #         experiment = experiment_i
-    #     ))
-    #     writeLines(output, file.path(exp_output_dir, paste0(experiment_i, ".qmd")))
-    # }
-
-    # # Add link to index
-    # rel_link <- file.path(crop_output_dir, "index.html")
-    # template_home <- c(template_home, paste0("- [", crop, "](", rel_link, ")"))    
 }
 
 # Generate table of crops for home template
